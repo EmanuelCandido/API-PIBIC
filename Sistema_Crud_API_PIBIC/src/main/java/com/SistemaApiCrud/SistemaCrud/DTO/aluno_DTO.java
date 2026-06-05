983a1a6931edeecb73501125925db0953809cb19
EmanuelCandido <emanuelcandidolima@gmail.com>
2026-06-04 22:15:03 -0300

    Alteração de configuração

--- Sistema_Crud_API_PIBIC/src/main/java/com/SistemaApiCrud/SistemaCrud/DTO/aluno_DTO.java
+++ Sistema_Crud_API_PIBIC/src/main/java/com/SistemaApiCrud/SistemaCrud/DTO/aluno_DTO.java
@@ -1,2 +1,21 @@
+package com.SistemaApiCrud.SistemaCrud.DTO;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+
 public class aluno_DTO {
+    private Long idAluno;
+    private String nome;
+    private String email;
+    private String curso;
+    private String periodo;
+
 }
