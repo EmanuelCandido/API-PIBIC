987afbccbf70a192ae5f74afa596663e95563a99
EmanuelCandido <emanuelcandidolima@gmail.com>
2026-06-16 22:13:01 -0300

    consertando

---
+++ Sistema_Crud_API_PIBIC/src/main/java/com/SistemaApiCrud/SistemaCrud/DTO/professor_cadastro_request_DTO.java
@@ -0,0 +1,33 @@
+package com.SistemaApiCrud.SistemaCrud.DTO;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class professor_cadastro_request_DTO {
+
+    @NotBlank(message = "O nome do professor e obrigatorio")
+    private String nome;
+
+    @NotBlank(message = "E-mail obrigatorio")
+    @Email(message = "E-mail invalido")
+    private String email;
+
+    @NotBlank(message = "A materia e obrigatoria")
+    private String materia;
+
+    @NotBlank(message = "O usuario e obrigatorio")
+    private String username;
+
+    @NotBlank(message = "A senha e obrigatoria")
+    @Size(min = 6, message = "A senha deve ter pelo menos 6 caracteres")
+    private String senha;
+}
